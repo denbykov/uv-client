cccfa9834f952c2c02be768ac3294792fb15531b
denbykov <jerkinston@gmail.com>
2025-04-22 17:14:54 +0300

    Put callbacks back into the component to avoid parameter redundancy

--- src/components/files-view.jsx
+++ src/components/files-view.jsx
@@ -28,6 +28,10 @@ class PaginationData {
     this.total = 0
 
     this.pagesLoaded = 0
+
+    this.previousPage = null
+    this.currentPage  = null
+    this.nextPage     = null
   }
 }
 
@@ -47,87 +51,121 @@ class State {
   }
 }
 
-async function handleMessage(stateRef, setItems, lastMessage) {
-  if (lastMessage === null) {
-    return;
-  }
-
-  const state = stateRef.current;
-
-  const message = await protocol.parseMessage(lastMessage);
-  if (state.uuid !== message.header.uuid) {
-    console.log("Not my uuid!");
-    return;
-  }
-
-  if (message.header.type == protocol.types.GetFilesResponse) {
-    const newItems = message.payload.files;
-    setItems(newItems);
-
-    state.pagination.total = message.payload.total;
-    state.pagination.pagesLoaded += 1;
-  }
-  else if (message.header.type === protocol.types.Error) {
-    console.log(`failed to load files: ${message.payload.reason}`);
-  } else {
-    console.log(`unable to handle message: ${message}`);
-  }
-}
-
-function loadPage(stateRef, sendMessage, page) {
-  if (page !== 0 && (page > 1 || page < -1)) {
-    throw new Error("Page argument should be in range [-1, 1]");
-  }
-
-  const state = stateRef.current;
-  state.uuid = protocol.uuidv4();
-
-  state.pagination.offset += state.pagination.limit * page;
-
-  const request = protocol.buildGetFilesRequest(
-    state.uuid, state.pagination.limit, state.pagination.offset);
-
-  sendMessage(request.serialize(), []);
-}
-
-function handleScroll(stateRef, event) {
-  const scrollTop = event.currentTarget.scrollTop;
-  const offsetHeight = event.currentTarget.offsetHeight;
-  const scrollHeight = event.currentTarget.scrollHeight;
-
-  const state = stateRef.current;
-
-  if (state.pagination.total > 0 && state.scrolling.itemHeight == 0) {
-    state.scrolling.itemHeight = 
-      scrollHeight / 
-      Math.min(state.pagination.limit, state.pagination.total);
-  }
-
-  // if (state.pagination.total > state.pagination.limit && state.pagination.pagesLoaded == 1) {
-  //   // loadPage();
-  // }
-
-  state.scrolling.scrollTop = scrollTop;
-  state.scrolling.offsetHeight = offsetHeight;
-
-  // console.log(scrollTop);
-  // console.log(offsetHeight);
-};
-
 export function FilesView() {
   const { lastMessage, sendMessage } = useWebSocketData();
-  const state = useRef(new State());
+  const stateRef = useRef(new State());
   const [items, setItems] = useState([]);
 
-  useEffect(() => {handleMessage(state, setItems, lastMessage)}, [lastMessage]);
-  useEffect(() => loadPage(state, sendMessage, 0), []);
+  // Methods
+
+  const handleFilesMessage = useCallback(
+    function(message) {
+      const state = stateRef.current;
+      const pagination = state.pagination;
+    
+      const newItems = message.payload.files;
+      setItems(newItems);
+      
+      state.pagination.total = message.payload.total;
+      const nextPageAvailable = pagination.offset + pagination.limit < pagination.total; 
+    
+      if (state.pagination.nextPage === null && nextPageAvailable) {
+        // loadPage(+1);
+      }
+    
+      state.pagination.pagesLoaded += 1;
+    },
+    []
+  );
+  
+  const handleMessage = useCallback(
+    async function(lastMessage) {
+      if (lastMessage === null) {
+        return;
+      }
+    
+      const state = stateRef.current;
+    
+      const message = await protocol.parseMessage(lastMessage);
+      if (state.uuid !== message.header.uuid) {
+        console.log("Not my uuid!");
+        return;
+      }
+    
+      if (message.header.type == protocol.types.GetFilesResponse) {
+        handleFilesMessage(message);
+      }
+      else if (message.header.type === protocol.types.Error) {
+        console.log(`failed to load files: ${message.payload.reason}`);
+      } else {
+        console.log(`unable to handle message: ${message}`);
+      }
+    },
+    []
+  );
+  
+  const loadPage = useCallback(
+    function (page) {
+      if (page !== 0 && (page > 1 || page < -1)) {
+        throw new Error("page argument should be in range [-1, 1]");
+      }
+    
+      const state = stateRef.current;
+      state.uuid = protocol.uuidv4(); 
+    
+      const offset = state.pagination.offset + state.pagination.limit * page;
+      const total = state.pagination.total;
+      if (offset < 0 || offset > total) {
+        throw new Error(`offset is out of the boundaries! offset: ${offset}, total: ${total}`)
+      }
+    
+      state.pagination.offset = offset;
+    
+      const request = protocol.buildGetFilesRequest(
+        state.uuid, state.pagination.limit, state.pagination.offset);
+    
+      sendMessage(request.serialize(), []);
+    },
+    []
+  );
+    
+  function handleScroll(event) {
+    const scrollTop = event.currentTarget.scrollTop;
+    const offsetHeight = event.currentTarget.offsetHeight;
+    const scrollHeight = event.currentTarget.scrollHeight;
+  
+    const state = stateRef.current;
+  
+    if (state.pagination.total > 0 && state.scrolling.itemHeight == 0) {
+      state.scrolling.itemHeight = 
+        scrollHeight / 
+        Math.min(state.pagination.limit, state.pagination.total);
+    }
+  
+    // if (state.pagination.total > state.pagination.limit && state.pagination.pagesLoaded == 1) {
+    //   // loadPage();
+    // }
+  
+    state.scrolling.scrollTop = scrollTop;
+    state.scrolling.offsetHeight = offsetHeight;
+  
+    // console.log(scrollTop);
+    // console.log(offsetHeight);
+  };
+
+  // Effects
+
+  useEffect(() => {handleMessage(lastMessage)}, [lastMessage]);
+  useEffect(() => loadPage(0), []);
+
+  // Rendering
 
   return (
     <>
       <div className="main-section">
         <div className="main sub-section">
         </div>
-        <div className="side sub-section scrollable" onScroll={(event) => {handleScroll(state, event)}}>
+        <div className="side sub-section scrollable" onScroll={(event) => {handleScroll(event)}}>
           <ul>
             {items.map((item, index) => (
               <FileListItem key={item.id} index={index} item={item} />
